81b576c023bee34183e330d47828a387d50d0d11
seth <sealittl@ucsc.edu>
2018-02-17 18:18:52 -0800

    Initial Commit

---
+++ MemeMatcher/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MemeMatcher
+//
+//  Created by Seth Little on 2/17/18.
+//  Copyright © 2018 Seth Little. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
