540b0e6c33b4987d0e4d78ab68342eaf885d6678
Zach Smith <zzachsmith@gmail.com>
2018-02-19 19:56:04 -0800

    commit changes

--- MemeMatcher/FourthViewController.swift
+++ MemeMatcher/FourthViewController.swift
@@ -15,6 +15,7 @@ class FourthViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
