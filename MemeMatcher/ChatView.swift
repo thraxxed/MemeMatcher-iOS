1de41ea28579abd4a5a62d1820a6c1506439c630
seth <sealittl@ucsc.edu>
2018-02-24 14:58:36 -0800

    nice

--- MemeMatcher/ChatView.swift
+++ MemeMatcher/ChatView.swift
@@ -55,6 +55,13 @@ class ChatView: UIViewController {
         case failure(Error)
     }
     
+    struct formMessage: Codable {
+        let body: String
+        init(body: String) {
+            self.body = body
+        }
+    }
+    
     func getMessages(for id: Int, completion: ((Result<[Message]>) -> Void)?) {
         var urlComponents = URLComponents()
         urlComponents.scheme = "https"
@@ -97,6 +104,52 @@ class ChatView: UIViewController {
     //MARK: Actions
     
     @IBAction func postMessage(_ sender: UITapGestureRecognizer) {
+        print(messageInputField.text!)
+        if (messageInputField.text == "") {
+            print("did not post message because input was empty")
+            return
+        }
+        sendPostMessage(message: formMessage(body: messageInputField.text!)) { (error) in
+            if let error = error {
+                fatalError(error.localizedDescription)
+            }
+        }
+    }
+    
+    func sendPostMessage(message: formMessage, completion:((Error?) -> Void)?){
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "meme-matcher.herokuapp.com"
+        urlComponents.path = "/api/messages"
+        guard let url = urlComponents.url else { fatalError("Could not create URL from components") }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        var headers = request.allHTTPHeaderFields ?? [:]
+        headers["Content-Type"] = "application/json"
+        request.allHTTPHeaderFields = headers
+        
+        
+        let encoder = JSONEncoder()
+        do {
+            let jsonData = try encoder.encode(message)
+            request.httpBody = jsonData
+            print("jsonData: ", String(data: request.httpBody!, encoding: .utf8) ?? "no body data")
+        } catch {
+            completion?(error)
+        }
+        
+        // Create and run a URLSession data task with our JSON encoded POST request
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let task = session.dataTask(with: request) { (responseData, response, responseError) in
+            guard responseError == nil else {
+                completion?(responseError!)
+                return
+            }
+        }
+        task.resume()
     }
     
 
